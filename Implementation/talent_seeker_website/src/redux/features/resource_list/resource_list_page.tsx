6657d1616b8acfb9d04aa63029c57db0f57cba16
AlfredoCh <alfredochsoliz@gmail.com>
2023-09-25 18:47:27 -0400

    updated resource list

--- Implementation/talent_seeker_website/src/redux/features/resource_list/resource_list_page.tsx
+++ Implementation/talent_seeker_website/src/redux/features/resource_list/resource_list_page.tsx
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import store, { RootState, useAppDispatch } from '../../store/store';
-import { fetchAllResourceItems } from './resource_list_slice';
+import { applyFilters, fetchAllResourceItems } from './resource_list_slice';
 
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import ResourceListComponent from '../../components/resourcec_list_component';
-import { Paper } from '@mui/material';
-import { ResourceItem } from '../../../data/models';
+import { IconButton, Paper } from '@mui/material';
+import { ResourceItem, SearchHomeItem } from '../../../data/models';
 import { selectResource } from '../resource_detail/resource_detail_slice';
 import { useNavigate } from 'react-router-dom';
+import SearchResourceComponent from '../../components/search_resource_component';
 
 export default function ResourceListPage() {
     const dispatch = useAppDispatch;
@@ -21,18 +22,59 @@ export default function ResourceListPage() {
         store.dispatch(fetchAllResourceItems());
     }, [dispatch]);
 
+    const [selectedValues, setSelectedValues] = useState<SearchHomeItem[]>([]);
+
+    const handleAutocompleteChange = (event: any, newValue: SearchHomeItem[]) => {
+        setSelectedValues(newValue);
+
+        const rolesIds = newValue.filter(e => e.objectType === "role").map(e => e.id);
+        const resourcesIds = newValue.filter(e => e.objectType === "resource").map(e => e.id);
+        const skillsIds = newValue.filter(e => e.objectType === "skill").map(e => e.id);
+
+        const filters = {
+            resourceIds: resourcesIds,
+            roleIds: rolesIds,
+            skillIds: skillsIds
+        }
+        store.dispatch(applyFilters(filters));
+        store.dispatch(fetchAllResourceItems());
+    };
+
+    //const dispatch = useAppDispatch;
+    const { items } = useSelector((state: RootState) => state.searchHome);
+
+    //addResourceToTeam
     function handleOnClickInfo(resource: ResourceItem): void {
         store.dispatch(selectResource(resource));
         navigate("/resourceDetail");
+
     }
 
-    return <Box sx={{ flexGrow: 0 }}>
-        <Paper elevation={2}>
-            <Grid container direction="column" spacing={0} alignItems="center"
-                justifyContent="center" sx={{ padding: '16px' }} >
-                <ResourceListComponent resourcesItems={resourceList} infoButtonConfiguration={{ isEnabled: true, action: handleOnClickInfo }}
-                    addButtonConfiguration={{ isEnabled: false, action: () => { } }} />
+
+    return <Grid xs  style={{padding:"16px"}}>
+        <Paper elevation={1}>
+            <Grid container direction="column"
+                justifyContent="center" >
+                <Grid xs>
+                    <SearchResourceComponent searchItems={items} selectedValues={selectedValues}
+                        searchButtonConfiguration={{
+                            isEnabled: false,
+                            searchButtonComponent: <IconButton
+                            >
+                            </IconButton>
+                        }}
+                        handleAutoCompleteChange={handleAutocompleteChange}
+                    />
+                </Grid>
+                <ResourceListComponent resourcesItems={resourceList} addButtonConfiguration={{
+                    isEnabled: false,
+                    action: () => { },
+                }}
+                    infoButtonConfiguration={{
+                        isEnabled: true,
+                        action: handleOnClickInfo,
+                    }} />
             </Grid>
         </Paper>
-    </Box>;
+    </Grid>;
 }
